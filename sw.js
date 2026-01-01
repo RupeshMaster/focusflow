395e8cb48940525db8d5e53221bd8e69eb8101fa
RupeshMaster <rupesh457809@gmail.com>
2026-01-01 16:51:49 +0530

    User Update: Increased navbar gap to 1.5rem & Cache Bump (reboot-v2).

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Service Worker for Offline Caching
  */
 
-const CACHE_NAME = 'focusflow-REBOOT-v1';
+const CACHE_NAME = 'focusflow-REBOOT-v2';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
