5e3f4a10401bb12af59a9c42c3a9f52795178a75
RupeshMaster <rupesh457809@gmail.com>
2026-01-01 17:01:59 +0530

    Layout Fix: Removed width:100% from nav-right to prevent flexbox conflict & Cache Bump (reboot-v3).

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Service Worker for Offline Caching
  */
 
-const CACHE_NAME = 'focusflow-REBOOT-v2';
+const CACHE_NAME = 'focusflow-REBOOT-v3';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
